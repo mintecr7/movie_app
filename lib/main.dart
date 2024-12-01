17c71eb4155889404c99e6e54217862436e6beef
Mintesnot Shiferaw <mintesnotshiferaw@gmail.com>
2024-12-02 00:33:39 +0900

    theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:movie_app/core/configs/theme/app_theme.dart';
 import 'package:movie_app/presentation/splash/pages/splash.dart';
 
@@ -11,6 +12,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.appTheme,
